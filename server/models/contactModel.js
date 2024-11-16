90b5ca6cefa5c8e1ab1d111f7f2ff08f8a1cc068
Sachin Jayadev <sachinjayadev18@gmail.com>
2024-11-16 15:58:07 +0530

    final

--- server/models/contactModel.js
+++ server/models/contactModel.js
@@ -18,7 +18,7 @@ const Contact = {
         return result.rows;
       },
     
-      update: async (id, firstName, lastName, email, phoneNumber, company, jobTitle) => {
+    update: async (id, firstName, lastName, email, phoneNumber, company, jobTitle) => {
         const query = `
           UPDATE "Contact"
           SET "firstName" = $1, "lastName" = $2, "email" = $3, "phoneNumber" = $4, "company" = $5, "jobTitle" = $6
@@ -30,7 +30,7 @@ const Contact = {
         return result.rows[0];
       },
     
-      delete: async (id) => {
+    delete: async (id) => {
         const query = 'DELETE FROM "Contact" WHERE id = $1 RETURNING *;';
         const result = await pool.query(query, [id]);
         return result.rows[0];
